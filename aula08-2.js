960548d197ecf914c5d3cd5679ba8ee4be6debe1
ayuizu <ayuizu0519@gmail.com>
2024-04-22 15:43:54 -0300

    v03

--- aula08-2.js
+++ aula08-2.js
@@ -72,7 +72,7 @@ console.log(nome2);
 let nomeAlterado = nome2.replace('Dick Vigarista','Pica Pau');
 console.log(nomeAlterado); //Pica Pau Medalha
 
-//substr  tá entrando em desuso, extrarir da posição inicial (7) posições adiante (5)
+//substr tá entrando em desuso, extrarir da posição inicial (7) posições adiante (5) //ragis
 let nome3='Dick Viragista Medalha';
 console.log(nome3.substr('7','5'));
 
@@ -84,4 +84,55 @@ console.log(nome2.toLocaleLowerCase());
 let nome4=" Batman ";
 console.log('-'+nome4.trim( )+'-');
 
-//AULA ATÉ 2H25
+//*Funções avançadas: Matemáticas
+// Documentação https://www.w3schools.com/java/java_math.asp
+
+//Arredonda para cima
+let x1 = Math.ceil(10.100); //11
+console.log("Para cima",x1);
+//Arredonda para cima
+let y1 = Math.floor(10.111); //10
+console.log("Para baixo",y1);
+//Arredonda para mais prox
+let z1 = Math.round(10.70); //11
+console.log("Mais prox",z1); 
+let w1 = Math.round(10.50); //11
+console.log("Mais prox",w1);
+let w2 = Math.round(10.30); //10
+console.log("Mais prox",w2);
+
+//Datas
+let data = new Date();
+
+console.log("Hoje é dia",data.getDate()); //conta do 1
+console.log("Estamos no mês", data.getMonth()+1); //conta do zero, pq? Pq a vida n é um morango
+console.log("Estamos no ano", data.getFullYear()); //conta do 1
+
+console.log("Estamos no dia",data.getDate(),"/",data.getMonth()+1,"/",data.getFullYear());
+console.log(data.toString()); //Imprime toda data em ingles, com data e fuso
+
+data.setDate(data.getDate()+720); 
+console.log(data.toString()); //Sun Apr 12 2026 15:29:46
+
+data.setFullYear(data.getFullYear()+5);
+console.log(data.toString()); //Sat Apr 12 2031 15:30:55
+
+console.log(data.getTime()); //1933785105275 data em miliseg (ms) desde 1/1/1970
+
+//Ver mais exemplos complexos no código disponibilizado pelo prof da aula
+
+//Em potrugues?
+// console.log(data.toString("pt-BR"));
+// console.log(data.toString("yyyy-MM-dd"));
+
+
+//*Exercicio 01: encontre a quantidade de milissegundos entre duas datas
+let data1 = new Date("2024-04-22");
+let data2 = new Date("2024-05-14");
+
+let milisegundosEntreDatas=Math.abs(data2.getTime()-data1.getTime());
+console.log(milisegundosEntreDatas); //1900800000 (ms)
+
+
+
+
